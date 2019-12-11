6985816a00cf7f17701e5c0805719fc08d651a51
pritamnegi-continuum <pritam.negi@continuum.net>
2019-12-11 15:39:45 +0530

    [C247C-1765] Added code for fetching Memory total width, data width in the library

--- smbios/decoder.go
+++ smbios/decoder.go
@@ -265,6 +265,15 @@ func (d *Decoder) next() (*Structure, error) {
 			}
 		}
 
+		if memInfo.TotalWidth > 0 {
+			totalWidth := uint64(memInfo.TotalWidth)
+			physicalMemory.TotalWidth = totalWidth
+		}
+		if memInfo.DataWidth > 0 {
+			dataWidth := uint64(memInfo.DataWidth)
+			physicalMemory.DataWidth = dataWidth
+		}
+
 		// Check for the size, if size is zero that means Empty DIMM Slot
 		if physicalMemory.SizeInBytes != 0 {
 			systemInfo.PhyMemory = append(systemInfo.PhyMemory, physicalMemory)
